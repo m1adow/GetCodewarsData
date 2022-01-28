1e867a7821c9c2e6899cba6db7d0760e1a5cee88
m1adow <Vell0505189970sl@gmail.com>
2022-01-28 21:58:11 +0200

    Add project files.

---
+++ GetCodewarsData/Models/CodeWars.cs
@@ -0,0 +1,48 @@
+﻿namespace GetCodewarsData.Models
+{
+    public class Overall
+    {
+        public int rank { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+        public int score { get; set; }
+    }
+
+    public class Csharp
+    {
+        public int rank { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+        public int score { get; set; }
+    }
+
+    public class Languages
+    {
+        public Csharp csharp { get; set; }
+    }
+
+    public class Ranks
+    {
+        public Overall overall { get; set; }
+        public Languages languages { get; set; }
+    }
+
+    public class CodeChallenges
+    {
+        public int totalAuthored { get; set; }
+        public int totalCompleted { get; set; }
+    }
+
+    public class Codewars
+    {
+        public string username { get; set; }
+        public string name { get; set; }
+        public int honor { get; set; }
+        public string clan { get; set; }
+        public int leaderboardPosition { get; set; }
+        public List<object> skills { get; set; }
+        public Ranks ranks { get; set; }
+        public CodeChallenges codeChallenges { get; set; }
+    }
+
+}
---
+++ GetCodewarsData/Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GetCodewarsData.Models;
+using System.Text.Json;
+using System.Net;
+
+namespace GetCodewarsData.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+        public string UserNickNameMessage { get; set; }
+        public string UserNameMessage { get; set; }
+        public string UserHonorMessage { get; set; }
+        public string UserRankMessage { get; set; }
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnPost(string? nickName)
+        {
+            if (nickName == null)
+                Message = "Write correct Nick Name";
+            else
+            {
+                WebClient webClient = new();
+                try
+                {
+                    string data = webClient.DownloadString($"https://www.codewars.com/api/v1/users/{nickName}");
+                    Codewars codewars = JsonSerializer.Deserialize<Codewars>(data);
+                    Message = "All common information: ";
+                    UserNickNameMessage = $"Nickname - {codewars.username};";
+                    UserNameMessage = $"Name - {codewars.name};";
+                    UserHonorMessage = $"Honor - {codewars.honor};";
+                    UserRankMessage = $"Rank - {codewars.ranks.overall.name}.";
+                }
+                catch (Exception ex)
+                {
+                    Message = ex.Message;
+                }
+            }                      
+        }
+    }
+}
